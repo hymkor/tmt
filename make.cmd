08c6b809b5ce4cef4445e45bc45ff80108243db8
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-08-16 14:59:34 +0900

    make.cmd: support `make package`

--- make.cmd
+++ make.cmd
@@ -5,11 +5,20 @@
 @exit /b
 
 :""
-    @for %%I in (386 amd64) do (
-        mkdir "%~dp0.\Cmd\%%~I"
-        set "GOARCH=%%~I"
-        go build -o "%~dp0.\Cmd\%%~I\tmt.exe"
-    )
+    @echo Usage: %~dpfx0 {package,clean}
+    @exit /b
+
+:"package"
+    @for %%I in (386 amd64) do call :package1 "%%I"
+    @exit /b
+
+:package1
+    @setlocal & set "GOARCH=%~1"
+    @set "DIR=%~dp0.\Cmd\%GOARCH%"
+    @for /F %%I in ('go env GOOS') do set "GOOS=%%I"
+    go build -o "%DIR%\tmt.exe"
+    zip -j9 "tmt-%DATE:/=%-%GOOS%-%GOARCH%.zip" "%DIR%\tmt.exe"
+    @endlocal
     @exit /b
 
 :"clean"
