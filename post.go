a7761539816a5feb41d8092a995519a9a598da42
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-12-14 23:48:15 +0900

    `tmt post` calls %EDITOR% if it is defined.

--- post.go
+++ post.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	tw "github.com/zetamatta/tmt/oauth"
 )
 
-func post(ctx context.Context, api *tw.Api, args []string) error {
-	text, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		return err
-	}
+var ByteOrderMark = []byte{0xEF, 0xBB, 0xBF}
 
-	_, err = api.PostTweet(string(text), nil)
+func post(ctx context.Context, api *tw.Api, args []string) error {
+	var text []byte
+	editor := os.Getenv("EDITOR")
+	if editor != "" {
+		fname := filepath.Join(os.TempDir(), "post.txt")
+		if err := ioutil.WriteFile(fname, ByteOrderMark, 066); err != nil {
+			return err
+		}
+		cmd := exec.Command(editor, fname)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		cmd.Run()
+		var buffer bytes.Buffer
+		fd, err := os.Open(fname)
+		if err != nil {
+			return err
+		}
+		defer fd.Close()
+		sc := bufio.NewScanner(fd)
+		for sc.Scan() {
+			line := sc.Bytes()
+			line = bytes.Replace(line, ByteOrderMark, []byte{}, -1)
+			buffer.Write(line)
+		}
+		if err := sc.Err(); err != nil {
+			return err
+		}
+		text = buffer.Bytes()
+	} else {
+		var err error
+		text, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+	}
+	_, err := api.PostTweet(string(text), nil)
 	return err
 }
