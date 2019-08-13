9c948116918cd87123c78e9f2af64a3b0dbed7c5
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-08-13 18:41:45 +0900

    `make update` replaces tmt.exe in %PATH%

--- make.cmd
+++ make.cmd
@@ -1,26 +1,31 @@
-@setlocal
-@set "PROMPT=$G "
-@call :"%1"
-@endlocal
-@exit /b
+@echo off
+setlocal
+set "PROMPT=$G "
+call :"%1"
+endlocal
+exit /b
 
 :""
-    @echo Usage: %~dpfx0 {package,clean}
-    @exit /b
+    echo Usage: %~dpfx0 {package,clean}
+    exit /b
 
 :"package"
-    @for %%I in (386 amd64) do call :package1 "%%I"
-    @exit /b
+    for %%I in (386 amd64) do call :package1 "%%I"
+    exit /b
 
 :package1
-    @setlocal & set "GOARCH=%~1"
-    @set "DIR=%~dp0.\Cmd\%GOARCH%"
-    @for /F %%I in ('go env GOOS') do set "GOOS=%%I"
+    setlocal & set "GOARCH=%~1"
+    set "DIR=%~dp0.\Cmd\%GOARCH%"
+    for /F %%I in ('go env GOOS') do set "GOOS=%%I"
     go build -o "%DIR%\tmt.exe"
     zip -j9 "tmt-%DATE:/=%-%GOOS%-%GOARCH%.zip" "%DIR%\tmt.exe"
-    @endlocal
-    @exit /b
+    endlocal
+    exit /b
+
+:"update"
+    for /F "skip=1" %%I in ('where tmt.exe') do copy /-Y tmt.exe "%%I"
+    exit /b
 
 :"clean"
     rmdir /s Cmd
-    @exit /b
+    exit /b
