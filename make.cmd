3dfcf02cecfd91deae19a9690691ede89e4d5d0e
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-09-29 22:10:57 +0900

    Implement `make package` for linux/windows x amd64/386

--- make.cmd
+++ make.cmd
@@ -10,15 +10,18 @@ exit /b
     exit /b
 
 :"package"
-    for %%I in (386 amd64) do call :package1 "%%I"
+    for %%I in (386 amd64) do call :package1 windows "%%I" .exe
+    for %%I in (386 amd64) do call :package1 linux   "%%I"
     exit /b
 
 :package1
-    setlocal & set "GOARCH=%~1"
-    set "DIR=%~dp0.\Cmd\%GOARCH%"
-    for /F %%I in ('go env GOOS') do set "GOOS=%%I"
-    go build -o "%DIR%\tmt.exe"
-    zip -j9 "tmt-%DATE:/=%-%GOOS%-%GOARCH%.zip" "%DIR%\tmt.exe"
+    setlocal
+    set "GOOS=%~1"
+    set "GOARCH=%~2"
+    for /F %%I in ('cd') do set "NAME=%%~nI"
+    set "SUFFIX=%~3"
+    go build
+    zip -9m "%NAME%-%DATE:/=%-%GOOS%-%GOARCH%.zip" "%NAME%%SUFFIX%"
     endlocal
     exit /b
 
